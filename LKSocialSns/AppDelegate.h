03acf0f694bc8716481459dc3f0a5d06410d7690
karos <329847738@qq.com>
2017-06-21 20:59:28 +0800

    Initial Commit

---
+++ LKSocialSns/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LKSocialSns
+//
+//  Created by karos li on 2017/6/21.
+//  Copyright © 2017年 lefit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LKSocialSns/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LKSocialSns
+//
+//  Created by karos li on 2017/6/21.
+//  Copyright © 2017年 lefit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
