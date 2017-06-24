f50f2dd754d91ac5b59181fdf230385a299f5a7f
karos <329847738@qq.com>
2017-06-24 17:22:17 +0800

    remove

--- uploadRepo.sh
+++ uploadRepo.sh
@@ -174,7 +174,7 @@ function checkInAndMakeTag() {
 ################ Upload function ############################
 function upload() {
 	local podspecFile=$1
-	pod repo push ${privatePods} ${podspecFile} --verbose --allow-warnings --use-libraries
+	pod repo push ${privatePods} ${podspecFile} --verbose --allow-warnings
 }
 
 ################# End upload function ############################
